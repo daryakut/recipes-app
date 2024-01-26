7f72955314fbd976a65fe2ac1363ac18db9b234f
daria kutluieva <daryakutlueva@gmail.com>
2024-01-26 18:14:48 +0100

    added useCallback

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,71 +1,61 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Recipe from "./Recipe";
 
 export default function Recipes({ recipes }) {
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
+  const iAmGosha = useCallback(() => {
+    console.log("hi");
+  }, []);
+
+  const showNextCard = useCallback(() => {
+    if (index >= recipes.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex((prev) => prev + 1);
+    }
+  }, [recipes.length, index]);
+
+  const showPreviousCard = useCallback(() => {
+    if (index <= 0) {
+      setIndex(recipes.length - 1);
+    } else {
+      setIndex((prev) => prev - 1);
+    }
+  }, [recipes.length, index]);
+
+  const handleKeyPress = useCallback(
+    (event) => {
       if (event.key === "ArrowRight") {
         showNextCard();
       } else if (event.key === "ArrowLeft") {
         showPreviousCard();
       }
-    };
+    },
+    [showNextCard, showPreviousCard]
+  );
 
+  useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
 
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, [showNextCard, showPreviousCard]);
-
-  console.log(recipes);
-
-  function showNextCard() {
-    if (index + 1 >= recipes.length) {
-      setIndex(0);
-    } else {
-      setIndex((prev) => prev + 1);
-    }
-  }
-  function showPreviousCard() {
-    if (index - 1 < 0) {
-      setIndex(recipes.length - 1);
-    } else {
-      setIndex((prev) => prev - 1);
-    }
-  }
+  }, [handleKeyPress]);
 
   return (
     <div className="container">
       <Recipe key={recipes[index].id} recipe={recipes[index]} />
       <div className="btns">
         {" "}
-        <button className="btn" onClick={showNextCard}>
+        <button className="btn" onClick={showPreviousCard}>
           Previous
         </button>
-        <button className="btn" onClick={showPreviousCard}>
+        <button className="btn" onClick={showNextCard}>
           Next
         </button>
       </div>
     </div>
   );
 }
-
-// const o = { x: 1, y: 2 };
-// const x = 5;
-// const n = 1;
-// function f1(input) {
-//   console.log(input.x);
-// }
-
-// const o2 = { n }; // { n: n }
-// const { y } = o;
-
-// function f2({ x }) {
-//   console.log(x);
-// }
-// f2(o); // prints 1
-// f2({x}); // prints 5
